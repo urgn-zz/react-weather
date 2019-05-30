cfdfe8e00cfd02a3c9754c1b7817cabe21f69a00
urgn <yuriy.raychinets@gmail.com>
2019-05-30 17:59:48 +0200

    Updates

---
+++ precache-manifest.78a7f5cae5b2b1df7b325194d54647df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e3f308a265c76343ec83919b70fb97",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "f6c825501e6d4162f10f",
+    "url": "/react-weather/static/css/main.1b9eaaff.chunk.css"
+  },
+  {
+    "revision": "c79cc24bec0458923ab9",
+    "url": "/react-weather/static/js/2.796a04e7.chunk.js"
+  },
+  {
+    "revision": "f6c825501e6d4162f10f",
+    "url": "/react-weather/static/js/main.21a9a141.chunk.js"
+  },
+  {
+    "revision": "e61e452087af7072f6e9",
+    "url": "/react-weather/static/js/runtime~main.2302d1fd.js"
+  }
+]);
